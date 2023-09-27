f7fd0540d8eb9ac7a0cd1d6e130cb0d07c2db19f
DimasIlham30 <midxinnn@gmail.com>
2023-09-27 11:17:03 +0700

    First

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,102 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { themeColor, useTheme } from "react-native-rapi-ui";
+import TabBarIcon from "../components/utils/TabBarIcon";
+import TabBarText from "../components/utils/TabBarText";
+
+import Pemasukan from "../screens/Pemasukan";
+import Pengaturan from "../screens/Pengaturan";
+import SecondScreen from "../screens/SecondScreen";
+import Detail from "../screens/Detail";
+import Pengeluaran from "../screens/Pengeluaran";
+
+const MainStack = createNativeStackNavigator();
+const Main = () => {
+  return (
+    <MainStack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <MainStack.Screen name="MainTabs" component={MainTabs} />
+      <MainStack.Screen name="SecondScreen" component={SecondScreen} />
+    </MainStack.Navigator>
+  );
+};
+
+const Tabs = createBottomTabNavigator();
+const MainTabs = () => {
+  const { isDarkmode } = useTheme();
+  return (
+    <Tabs.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          borderTopColor: isDarkmode ? themeColor.dark100 : "#c0c0c0",
+          backgroundColor: isDarkmode ? themeColor.dark200 : "#ffffff",
+        },
+      }}
+    >
+      {/* these icons using Ionicons */}
+      <Tabs.Screen
+        name="Pemasukan"
+        component={Pemasukan}
+        options={{
+          tabBarLabel: ({ focused }) => (
+            <TabBarText focused={focused} title="Pemasukan" />
+          ),
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={"cash"} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Pengeluaran"
+        component={Pengeluaran}
+        options={{
+          tabBarLabel: ({ focused }) => (
+            <TabBarText focused={focused} title="Pengeluaran" />
+          ),
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={"remove-circle-outline"} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Detail"
+        component={Detail}
+        options={{
+          tabBarLabel: ({ focused }) => (
+            <TabBarText focused={focused} title="Detail" />
+          ),
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={"information-circle"} />
+          ),
+        }}
+      />
+       <Tabs.Screen
+        name="Pengaturan"
+        component={Pengaturan}
+        options={{
+          tabBarLabel: ({ focused }) => (
+            <TabBarText focused={focused} title="Pengaturan" />
+          ),
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={"settings-outline"} />
+          ),
+        }}
+      />
+    </Tabs.Navigator>
+  );
+};
+
+export default () => {
+  return (
+    <NavigationContainer>
+      <Main />
+    </NavigationContainer>
+  );
+};
